21a4bc6c0bd9cdfa3cad4276c0e265ecb08c876e
EinsiTang <my-tangjianbin@163.com>
2025-07-07 08:25:18 +0800

    feat: add gin-security example
    - Add example project for gin-security usage
    - Create main.go with sample authentication and authorization logic
    - Include .gitignore file for project
    - Add go.mod and go.sum files for dependencies

---
+++ example/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	ginse "github.com/einsitang/gin-security"
+	"github.com/einsitang/go-security"
+	"github.com/gin-gonic/gin"
+)
+
+type ExamplePrincipal struct {
+	id    string
+	roles []string
+}
+
+func (e *ExamplePrincipal) Groups() []string {
+	return nil
+}
+func (e *ExamplePrincipal) Id() string {
+	return e.id
+}
+func (e *ExamplePrincipal) Permissions() []string {
+	return nil
+}
+func (e *ExamplePrincipal) Roles() []string {
+	return e.roles
+}
+
+func main() {
+	gse, _ := ginse.New()
+
+	// Pricipal Handler
+	gse.DoPrincipalHandler(func(c *gin.Context) (security.SecurityPrincipal, map[string]any, error) {
+
+		// tokenName := "Authorization"
+		// var tokenValue string
+
+		// // from cookie
+		// tokenValueFromCookie, err := c.Cookie(tokenName)
+		// if err != nil {
+		// 	return nil, nil, err
+		// }
+		// tokenValue = tokenValueFromCookie
+
+		// // from header
+		// tokenValue = c.GetHeader(tokenName)
+		// if tokenName == "" {
+		// 	return nil, nil, errors.New("not found token")
+		// }
+		// tokenValue, isFound := strings.CutPrefix(tokenValue, "Bearer ")
+		// if !isFound {
+		// 	return nil, nil, errors.New("token format error")
+		// }
+
+		// jwt parse
+		// jwtUtils.parse(tokenValue)
+
+		// read session from storage
+		// redis
+		// mysql
+		// memory
+
+		// for demo
+		principal := &ExamplePrincipal{
+			id: "test",
+			roles: []string{
+				"admin",
+			},
+		}
+		return principal, nil, nil
+	})
+
+	// 注册 401 hander 认证失败 handler
+	// gse.DoUnauthorizedHandler(func(c *gin.Context) {
+	// 	c.JSON(401, gin.H{
+	// 		"message": "unauthorized",
+	// 	})
+	// 	c.Abort()
+	// })
+
+	// // 注册 403 hander 权限验证失败 handler
+	// gse.DoForbiddenHandler(func(c *gin.Context) {
+	// 	c.JSON(403, gin.H{
+	// 		"message": "forbidden",
+	// 	})
+	// 	c.Abort()
+	// })
+
+	r := gin.Default()
+	r.Use(gin.Recovery())
+	// r.Use(se.WithPartol())
+
+	r.POST("/v1/api/login", func(c *gin.Context) {
+		username := "hello"
+		param := map[string]any{
+			"username": username,
+		}
+		_ = param
+		expireTime := -1
+		_ = expireTime
+		accessToken := "" // jwt
+
+		c.JSON(200, gin.H{
+			"accessToken": accessToken,
+		})
+	})
+	express := "allow:Role('admin') and $age > 18"
+	r.GET("/v1/api/users", gse.WithGuard(express), func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"helo": "world",
+		})
+	})
+
+	log.Fatal(r.Run())
+
+}
---
+++ security.go
@@ -0,0 +1,186 @@
+package ginse
+
+import (
+	"log"
+	"slices"
+
+	security "github.com/einsitang/go-security"
+	"github.com/gin-gonic/gin"
+)
+
+type GinSe interface {
+	// return white list , white list will skip check
+	WhiteList() []string
+
+	// gin global middleware
+	WithPartol() gin.HandlerFunc
+
+	// gin endpoint middleware
+	WithGuard(express string) gin.HandlerFunc
+
+	// forbidden handler (403)
+	//
+	// if forbidden handler is not set, will use default handler
+	DoForbiddenHandler(gin.HandlerFunc)
+
+	// unauthorized handler (401)
+	//
+	// if unauthorized handler is not set, will use default handler
+	DoUnauthorizedHandler(gin.HandlerFunc)
+
+	DoPrincipalHandler(DoPrincipalHandler)
+}
+
+type DoPrincipalHandler func(c *gin.Context) (security.SecurityPrincipal, map[string]any, error)
+
+type ginSe struct {
+	TokenName      string
+	AuthFromHeader bool
+
+	partol    security.Partol
+	whiteList []string
+
+	// callback handlers
+
+	forbiddenHandler    gin.HandlerFunc
+	unauthorizedHandler gin.HandlerFunc
+	principalHandler    DoPrincipalHandler
+}
+
+func (g *ginSe) WhiteList() []string {
+	return g.whiteList
+}
+
+func (g *ginSe) WithPartol() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		fullpath := c.FullPath()
+		if slices.Contains(g.WhiteList(), fullpath) {
+			c.Next()
+			return
+		}
+
+		// 恢复 principal
+		principal, customParam, err := g.principalHandler(c)
+
+		if err != nil {
+			// 401
+			log.Println(err)
+			g.unauthorizedHandler(c)
+			return
+		}
+
+		_ = customParam
+
+		if principal == nil {
+			// 401
+			g.unauthorizedHandler(c)
+			return
+		}
+
+		chekced, _ := g.partol.Check(fullpath, principal)
+		if chekced {
+			c.Next()
+		} else {
+			g.forbiddenHandler(c)
+		}
+	}
+}
+
+func (g *ginSe) WithGuard(express string) gin.HandlerFunc {
+	guard, err := security.NewGuard(express)
+	if err != nil {
+		log.Fatal(err)
+		panic(err.Error())
+	}
+	return func(c *gin.Context) {
+		params := map[string]any{}
+		// for _, param := range c.Params {
+		// 	params[param.Key] = param.Value
+		// }
+		for k, vs := range c.Request.URL.Query() {
+			params[k] = vs[0]
+		}
+
+		// 恢复 principal
+		principal, customParam, err := g.principalHandler(c)
+		if err != nil {
+			// 401
+			log.Println(err)
+			g.unauthorizedHandler(c)
+			return
+		}
+		if principal == nil {
+			// 401
+			g.unauthorizedHandler(c)
+			return
+		}
+		_ = customParam
+		var context = &security.SecurityContext{
+			Principal: principal,
+			Params:    params,
+		}
+		checked, err := guard.Check(context)
+		if err != nil {
+			log.Println(err)
+			// 401
+			g.unauthorizedHandler(c)
+			return
+		}
+		if checked {
+			c.Next()
+		} else {
+			// 403
+			g.forbiddenHandler(c)
+		}
+
+	}
+}
+
+func (g *ginSe) DoForbiddenHandler(h gin.HandlerFunc) {
+	g.forbiddenHandler = h
+}
+
+func (g *ginSe) DoUnauthorizedHandler(h gin.HandlerFunc) {
+	g.unauthorizedHandler = h
+}
+
+func (g *ginSe) DoPrincipalHandler(h DoPrincipalHandler) {
+	g.principalHandler = h
+}
+
+func New() (GinSe, error) {
+	partol, err := security.NewPartol()
+	if err != nil {
+		return nil, err
+	}
+
+	tokenName := "Authorization"
+	authFromHeader := true
+	whiteList := []string{}
+
+	se := &ginSe{TokenName: tokenName, AuthFromHeader: authFromHeader, partol: partol, whiteList: whiteList}
+	se.forbiddenHandler = defaultForbiddenHandler
+	se.unauthorizedHandler = defaultUnauthorizedHandler
+	se.principalHandler = defaultPrincipalHandler
+
+	return se, nil
+}
+
+func defaultPrincipalHandler(c *gin.Context) (security.SecurityPrincipal, map[string]any, error) {
+	panic("not implement principalHandler")
+}
+
+func defaultForbiddenHandler(c *gin.Context) {
+	c.JSON(403, gin.H{
+		"message": "forbidden",
+	})
+	c.Abort()
+}
+
+func defaultUnauthorizedHandler(c *gin.Context) {
+	c.JSON(401, gin.H{
+		"message": "unauthorized",
+	})
+	c.Abort()
+}
